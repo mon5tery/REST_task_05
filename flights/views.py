742b77adc4ecfc49b787e6672e430c06c3d7db9e
ahalrasheed <ahmed.alrasheed88@gmail.com>
2019-07-25 19:00:25 +0300

    I don't know where to start in the error after test.

--- flights/views.py
+++ flights/views.py
@@ -2,18 +2,24 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdate
 from datetime import datetime
 
 from .models import Flight, Booking
-from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterSerializer
+from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterSerializer, AdminSerializer, UserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 
 
 class FlightsList(ListAPIView):
 	queryset = Flight.objects.all()
 	serializer_class = FlightSerializer
+	permission_classes = [AllowAny]
 
 
 class BookingsList(ListAPIView):
-	queryset = Booking.objects.filter(date__gte=datetime.today())
+	# queryset = Booking.objects.filter(date__gte=datetime.today(), user=request.user)
 	serializer_class = BookingSerializer
+	permission_classes = [IsAuthenticated]
 
+	def get_queryset(self):
+	 	self.serializer_class
+	 	return Booking.objects.filter(date__gte=datetime.today(), user=self.request.user)
 
 class BookingDetails(RetrieveAPIView):
 	queryset = Booking.objects.all()
@@ -28,11 +34,20 @@ class UpdateBooking(RetrieveUpdateAPIView):
 	lookup_field = 'id'
 	lookup_url_kwarg = 'booking_id'
 
+	def Admin(RetrieveUpdateAPIView):
+		serializer_class = AdminSerializer
+		if self.request.user.is_staff:
+			serializer_class = AdminSerializer
+		else:
+			serializer_class = UserSerializer
+		return serializer_class
+
 
 class CancelBooking(DestroyAPIView):
 	queryset = Booking.objects.all()
 	lookup_field = 'id'
 	lookup_url_kwarg = 'booking_id'
+	permission_classes = [IsAuthenticated, IsAdminUser]
 
 
 class BookFlight(CreateAPIView):
@@ -44,3 +59,5 @@ class BookFlight(CreateAPIView):
 
 class Register(CreateAPIView):
 	serializer_class = RegisterSerializer
+
+
